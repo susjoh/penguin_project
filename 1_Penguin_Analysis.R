91a674c1cfe7574c12770dd243648ac8a4a0551a
susjoh <sjohnston84@gmail.com>
2023-09-26 12:06:27 +0100

    First commit

---
+++ 1_Penguin_Analysis.R
@@ -0,0 +1,37 @@
+#
+# Palmer Penguins Analysis
+# Susan Johnston
+# September 2023
+
+# Load the tidyverse library.
+
+library(tidyverse)   # if it doesn't exist, run install.packages("tidyverse").
+# You only need to do this once.
+
+# Read in the data.
+
+penguins <- read_table("data/penguin_data.txt")
+glimpse(penguins)
+
+# Run a linear regression.
+
+model1 <- lm(body_mass_g ~ flipper_length_mm, data = penguins)
+summary(model1)
+
+# Create a nice plot in ggplot2.
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) +
+  geom_point() +
+  stat_smooth(method = "lm")
+
+# Save the plot in your /figs folder.
+
+ggsave("figs/1_flipper_bodymass_regression.png")
+
+# Subset the data to only analyses females.
+
+penguins_female <- subset(penguins, sex == "female")
+
+# Save the edited dataset.
+
+write_tsv(penguins_female, "results/1_penguin_female_only.txt")
---
+++ README.md
@@ -0,0 +1,7 @@
+# Penguin Project
+
+This is a test project investigating morphometric traits on the Palmer penguin dataset.
+
+For more information on the Palmer penguin project, please see the link [here](https://allisonhorst.github.io/palmerpenguins/).
+
+Any questions, please email [Susan.Johnston\@ed.ac.uk](mailto:Susan.Johnston@ed.ac.uk){.email}
